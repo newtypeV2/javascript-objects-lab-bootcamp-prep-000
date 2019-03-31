98167d8fd9347cfcfb4032b6609873e9de70a91d
Gian Libunao <gianlibunao@gmail.com>
2019-03-31 19:47:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var recipes = {
 
 function updateObjectWithKeyAndValue(object,key,value){
   Object.assign({},object, {key: value})
+  return object
 }
 
 function updateObjectWithKeyAndValue(object, key, value){
