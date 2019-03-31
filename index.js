aab821f039e2b596337007fdfa822838e6b0b404
Gian Libunao <gianlibunao@gmail.com>
2019-03-31 20:01:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object, key){
-  delete object[key]
-  return object
+  var tempobj = Object.assign({},object)
+  delete tempobj[key]
+  return tempobj
 }
